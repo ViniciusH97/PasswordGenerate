dcd6f4c013b0da4875be5d9b773b733d631a07d0
Vinicius <vinicius.hl2010@gmail.com>
2024-08-13 20:16:15 -0300

    .

--- wordlist/passw1.js
+++ wordlist/passw1.js
@@ -2,5 +2,7 @@ module.exports = [
   '123456', 'password', '123456789', '12345678', '12345',
   '1234567', 'qwerty', 'abc123', '111111', 'password1',
   '1234', 'admin', 'letmein', 'welcome', 'monkey',
-  '123123', 'qwertyuiop', '654321', 'zxcvbnm', '1q2w3e4r'
+  '123123', 'qwertyuiop', '654321', 'zxcvbnm', '1q2w3e4r',
+  'senha123', 'senha321', 'nome', 'name', '0987654321', '654321',
+  '654', '123', 'abcde', 'abcd', 'abcdefg', 'senha1', 'senha2', 'senha3',
 ];
